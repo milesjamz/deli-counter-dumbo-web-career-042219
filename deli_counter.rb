e264abecfeab6342275642b3fa8a4ec51684d5c7
Miles Marillo <mjm03@hampshire.edu>
2019-03-31 17:56:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 katz_deli = []
 def line(katz_deli)
-  if katz_deli.length < 1
+  if katz_deli.length == 0
   puts "The line is currently empty."
 else
   puts "The line is currently:"
