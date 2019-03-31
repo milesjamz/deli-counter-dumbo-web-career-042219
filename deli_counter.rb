f6484564b84dc962453271c6f84c829690a3373f
Miles Marillo <mjm03@hampshire.edu>
2019-03-31 17:36:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def take_a_number(katz_deli, newbie)
   katz_deli.push("Welcome, #{newbie}! You are number #{number} on the line.")
 end
 def now_serving(katz_deli)
-  if katzdeli.length == 0
+  if katzdeli.length < 1
     puts "The line is currently empty."
   else puts "Currently serving #{name}."
   katz_deli.shift
