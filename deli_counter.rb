b5288245895419e70dad3b71b3d2beabba0bb95d
Miles Marillo <mjm03@hampshire.edu>
2019-03-31 17:23:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+katz_deli = []
+def line(katz_deli)
+  puts "The line is currently empty."
+end
+def take_a_number(katz_deli, newbie)
+  katz_deli.push("Welcome, #{newbie}! You are number #{number} on the line.")
+end
+def now_serving(katz_deli)
+  puts "Currently serving #{name}
+  katz_deli."
