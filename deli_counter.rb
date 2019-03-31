9bc90a04db3627c8c9e0137201724f73255d3fc9
Miles Marillo <mjm03@hampshire.edu>
2019-03-31 18:07:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
   if katz_deli.length == 0
   puts "The line is currently empty."
 else
-  katz_deli.each_with_index
+  katz_deli.each.with_index(1) 
   puts "The line is currently:"
   end
 end
