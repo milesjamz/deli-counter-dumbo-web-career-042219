c78f4768a3f20b10b3468391b96a681c9beb1a4b
Miles Marillo <mjm03@hampshire.edu>
2019-03-31 17:38:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ def take_a_number(katz_deli, newbie)
 end
 def now_serving(katz_deli)
   if katz_deli.length < 1
-    puts "The line is currently empty."
+    puts "There is nobody waiting to be served!"
   else puts "Currently serving #{name}."
   katz_deli.shift
 end
