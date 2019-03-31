d700a24a9f8136b1231e82cce091df25537e9cb6
Miles Marillo <mjm03@hampshire.edu>
2019-03-31 18:00:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,7 +10,7 @@ end
 
 def take_a_number(katz_deli, newbie)
   katz_deli.push(name)
-  puts "Welcome, #{newbie}! You are number #{katz_deli.length + 1} on the line."
+  puts "Welcome, #{newbie}! You are number #{katz_deli.length + 1} in line."
 end
 
 def now_serving(katz_deli)
