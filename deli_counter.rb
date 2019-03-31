f9a67c03b32262b83238d58a02bf577515ba1b92
Miles Marillo <mjm03@hampshire.edu>
2019-03-31 18:12:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,9 +6,9 @@ def line(katz_deli)
   puts "The line is currently empty."
 else
   katz_deli.each.with_index(1) do |name, index|
-  line_array.push("#{index}, #{name}")
+  newArray.push("#{index}, #{name}")
 end
-  puts "The line is currently: "
+  puts "The line is currently: #{newArray.
   end
 end
 
